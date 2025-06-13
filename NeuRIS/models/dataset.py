8c52fffee040bc0baf3dee8db0a2391c1aedf610
Justin Stephens <jtstephens18@gmail.com>
2025-06-13 15:28:19 -0500

    ray debug

--- NeuRIS/models/dataset.py
+++ NeuRIS/models/dataset.py
@@ -531,7 +531,8 @@ class Dataset:
         rays_v = p / torch.linalg.norm(p, ord=2, dim=-1, keepdim=True)    # batch_size, 3
         rays_v = torch.matmul(self.pose_all[img_idx, None, :3, :3], rays_v[:, :, None]).squeeze()  # batch_size, 3
         rays_o = self.pose_all[img_idx, None, :3, 3].expand(rays_v.shape) # batch_size, 3
-        return torch.cat([rays_o.cpu(), rays_v.cpu(), color, mask[:, :1]], dim=-1).cuda()    # batch_size, 10
+        #return torch.cat([rays_o.cpu(), rays_v.cpu(), color, mask[:, :1]], dim=-1).cuda()    # batch_size, 10
+        return torch.cat([rays_o.cpu(), rays_v.cpu(), color, mask[:, None]], dim=-1).cuda()
         
     def random_get_rays_at(self, img_idx, batch_size, pose = None):
         pose_cur = self.get_pose(img_idx, pose)
