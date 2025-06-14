8abeac8ce01685aa826c74603725ba5c52fe6af8
Justin Stephens <jtstephens18@gmail.com>
2025-06-14 07:23:07 -0500

    syntax

--- NeuRIS/models/dataset.py
+++ NeuRIS/models/dataset.py
@@ -180,7 +180,7 @@ class Dataset:
         #     self.pose_all.append(torch.from_numpy(pose).float())
 
         for cam_id, camera in self.cameras.items():
-            image = find_images_by_camera(camera["id"], self.images)
+            image = self.find_images_by_camera(camera["id"], self.images)
             cam, img = self.format_colmap_data_for_projection(camera, image)
             pose, intrinsics = compute_world_to_image_matrix(cam, img)
             if self.resolution_level > 1.0:
